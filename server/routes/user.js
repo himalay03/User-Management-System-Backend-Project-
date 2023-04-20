43d286910cb32ab475bedbcbc370f16541809ce6
himalay03 <114144373+himalay03@users.noreply.github.com>
2023-04-20 20:00:58 +0530

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,19 +3,13 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 // create, find, update, delete etc(functions)
-router.post("/", userController.find);
-router.post("/adduser", userController.create);
-router.post("/edituser/:id", userController.update);
-
 router.get("/", userController.view);
+router.post("/", userController.find);
 router.get("/adduser", userController.form);
+router.post("/adduser", userController.create);
 router.get("/edituser/:id", userController.edit);
-router.get("/:id", userController.delete);
+router.post("/edituser/:id", userController.update);
 router.get("/viewuser/:id", userController.viewall);
-
-// Router
-// router.get("", (req, res) => {
-//   res.render("home");
-// });
+router.get("/:id", userController.delete);
 
 module.exports = router;
